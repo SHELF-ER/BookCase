c8b6ea30e0fa9ade461859884e57f0b44a58803d
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-01 14:42:37 +0900

    10/1 - 스타일 변경

--- src/main/resources/static/js/clock.js
+++ src/main/resources/static/js/clock.js
@@ -1,5 +1,5 @@
-const clock = document.querySelector("h2#clock");
-
+const caldr = document.querySelector("#clock__caldr");
+const time = document.querySelector("#clock__time");
 
 function getClock(){
     const date  = new Date();
@@ -7,10 +7,12 @@ function getClock(){
     // const mon = String(date.getMonth('en-us', {2-digit})).padStart(2,"0");
     const mon = String(date.toLocaleString('ko-kr', {month: 'long'})).padStart(2,"0");
     const day = String(date.toLocaleString('ko-kr', {day: '2-digit'})).padStart(2,"0");
+    caldr.innerText = `${year} ${mon} ${day}`;
+    
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${year}:${mon}:${day}:${hours}:${minutes}:${seconds}`;
+    time.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
