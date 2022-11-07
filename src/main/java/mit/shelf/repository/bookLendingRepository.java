94bc5272917807b33e17b029e1f45c388327ad6f
woogi <dbwhddnr00@naver.com>
2022-11-07 20:21:08 +0900

    mapping 오류 수정

--- src/main/java/mit/shelf/repository/bookLendingRepository.java
+++ src/main/java/mit/shelf/repository/bookLendingRepository.java
@@ -3,12 +3,13 @@ package mit.shelf.repository;
 import mit.shelf.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface bookLendingRepository extends JpaRepository<Member, String> {
     @Query(value = "SELECT * FROM member WHERE borrower = :name ", nativeQuery = true)
-    List<Member>  findByBookLending(String name);
+    List<Member>  findByBookLending(@Param("name") String name);
 
 
 
